0f72afda0fc466d2cc673b0d8052e6ebe7e6e3c9
Johnny Sand <johnnysand7@gmail.com>
2016-07-18 16:36:01 -0700

    Adding test project files
    Playing with git in Windows 10

---
+++ test_file.py
@@ -0,0 +1,6 @@
+def a_function():
+	return "Good day!"
+
+
+if __name__ == "__main__":
+	print a_function()
---
+++ what_am_i_doing.py
@@ -0,0 +1,6 @@
+
+def whatever():
+    print "This seems to be working!"
+    
+if __name__ == "__main__":
+    whatever()
